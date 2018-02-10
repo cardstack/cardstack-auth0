64d01e184fc42e586855019e81ff655e0c341e2f
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-02-10 17:58:55 -0500

    move auth0 to a service

--- addon/components/auth0-login.js
+++ addon/components/auth0-login.js
@@ -1,64 +1,8 @@
 import Ember from 'ember';
 import layout from '../templates/components/auth0-login';
-import { configure, getConfiguration } from 'torii/configuration';
-import { task } from 'ember-concurrency';
-const { getOwner } = Ember;
 
 export default Ember.Component.extend({
   layout,
   tagName: '',
-  session: Ember.inject.service(),
-  torii: Ember.inject.service(),
-
-  source: 'auth0',
-
-  fetchConfig: task(function * () {
-    if (typeof FastBoot !== "undefined") { return; }
-
-    let { clientId, domain, toriiRemoteService, popup, redirectUri, scope } = yield getOwner(this).lookup('authenticator:cardstack').fetchConfig(this.get('source'));
-    let opts = {
-      'auth0-oauth2': {
-        baseUrl: `https://${domain}/authorize`,
-        apiKey: clientId,
-        scope,
-        redirectUri
-      }
-    };
-    if (toriiRemoteService) {
-      opts["auth0-oauth2"]["remoteServiceName"] = toriiRemoteService;
-    }
-    extendToriiProviders(opts);
-    this.set("popup", popup);
-  }).observes('source').on('init'),
-
-  login: task(function * () {
-    // this should wait for fetchConfig to be done, but if we block
-    // before opening the popup window we run afoul of popup
-    // blockers. So instead in our template we don't render ourself at
-    // all until after fetchConfig finishes. Fixing this more nicely
-    // would require changes to Torii.
-    let { authorizationCode } = yield this.get('torii').open('auth0-oauth2', this.get("popup") || {});
-
-    if (authorizationCode) {
-      yield this.get('authenticate').perform(authorizationCode);
-    }
-  }).drop(),
-
-  authenticate: task(function * (authorizationCode) {
-    yield this.get('session').authenticate('authenticator:cardstack', this.get('source'), { authorizationCode });
-  }),
-
-  cancelLogin: task(function * () {
-    this.get("login").cancelAll();
-    yield this.get('torii').close('auth0-oauth2');
-  }).drop(),
+  auth0: Ember.inject.service(),
 });
-
-function extendToriiProviders(newConfig) {
-  let toriiConfig = Object.assign({}, getConfiguration());
-  if (!toriiConfig.providers) {
-    toriiConfig.providers = {};
-  }
-  Object.assign(toriiConfig.providers, newConfig)
-  configure(toriiConfig);
-}
---
+++ app/services/auth0.js
@@ -0,0 +1,1 @@
+export { default } from 'cardstack-auth0/services/auth0';

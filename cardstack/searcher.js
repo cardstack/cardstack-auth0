aee7849d21e10802aef5c845924b57a62b281cf7
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-02-14 11:49:40 -0500

    update for cardstack hub 0.5.0 changes

--- cardstack/searcher.js
+++ cardstack/searcher.js
@@ -14,14 +14,14 @@ module.exports = class Auth0Searcher {
     this.dataSource = dataSource;
   }
 
-  async get(branch, type, id, next) {
+  async get(session, branch, type, id, next) {
     if (type === 'auth0-users') {
       return this._getUser(id);
     }
     return next();
   }
 
-  async search(branch, query, next) {
+  async search(session, branch, query, next) {
     return next();
   }
 

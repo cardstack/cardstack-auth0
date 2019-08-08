4a3ff4bd57e8394144e94de03b86cf933f8677ec
Casper Purtlebaugh <casperpurtlebaugh@gmail.com>
2019-08-07 23:49:06 -0700

    update code-generator.js to return new Map versus nothing

--- cardstack/code-generator.js
+++ cardstack/code-generator.js
@@ -16,43 +16,44 @@ class Auth0CodeGenerator {
   async generateModules() {
     let activeSources = await this.sources.active();
     let source = Array.from(activeSources.values()).find(s => s.sourceType === '@cardstack/auth0');
-    if (!source || !source.authenticator) { return; }
-
-    let { clientId,
-          domain,
-          scope,
-          toriiRemoteService,
-          appUrl,
-          popup,
-          forcePopupBrowserList
-    } = source.authenticator;
-    let properties = [{
-      name: 'clientId',
-      value: clientId,
-    },{
-      name: 'domain',
-      value: domain
-    },{
-      name: 'appUrl',
-      value: appUrl
-    },{
-      name: 'scope',
-      value: scope
-    },{
-      name: 'toriiRemoteService',
-      value: toriiRemoteService
-    },{
-      name: 'redirectUri',
-      value: `${appUrl}/torii/redirect.html`,
-    },{
-      name: 'popup',
-      value: popup
-    },{
-      name: 'forcePopupBrowserList',
-      value: (forcePopupBrowserList || []).join(',')
-    }];
-
-    let compiled = template({ properties });
-    return new Map([['environment', compiled]]);
+    if (!source || !source.authenticator) { 
+      return new Map()
+    } else {
+      let { clientId,
+        domain,
+        scope,
+        toriiRemoteService,
+        appUrl,
+        popup,
+        forcePopupBrowserList
+      } = source.authenticator;
+      let properties = [{
+        name: 'clientId',
+        value: clientId,
+      },{
+        name: 'domain',
+        value: domain
+      },{
+        name: 'appUrl',
+        value: appUrl
+      },{
+        name: 'scope',
+        value: scope
+      },{
+        name: 'toriiRemoteService',
+        value: toriiRemoteService
+      },{
+        name: 'redirectUri',
+        value: `${appUrl}/torii/redirect.html`,
+      },{
+        name: 'popup',
+        value: popup
+      },{
+        name: 'forcePopupBrowserList',
+        value: (forcePopupBrowserList || []).join(',')
+      }];
+      let compiled = template({ properties });
+      return new Map([['environment', compiled]]);
+    }
   }
 });

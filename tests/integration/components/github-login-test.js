c418586421781d6decc682230fbfb4931bdf0c3a
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-01 04:09:13 -0400

    Ran codemods to remove jQuery

--- tests/integration/components/github-login-test.js
+++ tests/integration/components/github-login-test.js
@@ -1,26 +1,27 @@
 import { find } from '@ember/test-helpers';
-import { moduleForComponent, skip } from 'ember-qunit';
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('auth0-login', 'Integration | Component | auth0 login', {
-  integration: true
-});
+module('Integration | Component | auth0 login', function(hooks) {
+  setupRenderingTest(hooks);
 
-skip('it renders', function(assert) {
+  skip('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{auth0-login}}`);
+    this.render(hbs`{{auth0-login}}`);
 
-  assert.equal(find('*').textContent.trim(), '');
+    assert.equal(find('*').textContent.trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#auth0-login}}
-      template block text
-    {{/auth0-login}}
-  `);
+    // Template block usage:
+    this.render(hbs`
+      {{#auth0-login}}
+        template block text
+      {{/auth0-login}}
+    `);
 
-  assert.equal(find('*').textContent.trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 });
